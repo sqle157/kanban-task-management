a3f59fcd7e3f8b87b6adaa2de66df4abba074093
sqle157 <sql@Sangs-MacBook-Pro.local>
2023-02-03 21:47:38 -0700

    FRONTEND: minor refactor to the code

--- frontend/src/components/KanbanComponents/TaskCard.tsx
+++ frontend/src/components/KanbanComponents/TaskCard.tsx
@@ -13,16 +13,18 @@ function TaskCard({ task }: TaskCardProps) {
 	}
 
 	return (
-		<button
-			className='mb-2 w-full cursor-pointer rounded-lg bg-[#2B2C37] px-4 py-6 text-start text-[.9375rem] font-bold leading-[19px] text-white shadow-md shadow-[rgba(54,78,126,0.101545)] hover:text-[#635FC7]'
-			type='button'
-			onClick={handleViewTaskClick}>
-			{task.title}
-			<span className='mt-5 block text-xs font-bold leading-[.9375rem] text-[#828FA3]'>
-				{task.subtasks.filter((subtask) => subtask.isCompleted).length} of{' '}
-				{task.subtasks.length} subtasks
-			</span>
-		</button>
+		<div className='mb-2 w-full cursor-pointer rounded-lg bg-[#2B2C37] px-4 py-6 text-start text-[.9375rem] font-bold leading-[19px] text-white shadow-md shadow-[rgba(54,78,126,0.101545)] hover:text-[#635FC7]'>
+			<button
+				type='button'
+				onClick={handleViewTaskClick}
+				className='h-full w-full text-start'>
+				{task.title}
+				<span className='mt-5 block text-xs font-bold leading-[.9375rem] text-[#828FA3]'>
+					{task.subtasks.filter((subtask) => subtask.isCompleted).length} of{' '}
+					{task.subtasks.length} subtasks
+				</span>
+			</button>
+		</div>
 	);
 }
 export default TaskCard;
