a2cee16d48d09c1b9595fb0b0c8802deae8a8d87
sqle157 <sql@Sangs-MacBook-Pro.local>
2023-02-08 12:03:18 -0700

    BACKEND: prepare for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import path from 'path';
 import { connectDB } from './config/db';
 import { errorHandler } from './middleware/errorMiddleware';
 // Routes
@@ -26,6 +27,23 @@ app.use('/api/tasks', taskRoutes);
 
 // middleware
 app.use(errorHandler);
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../../frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.join(__dirname, '../../frontend/build/index.html'))
+	);
+} else {
+	// development mode
+	app.get('/', (req, res) => {
+		res
+			.status(200)
+			.json({ message: 'Welcome to the Kanban Task Management WebApp API' });
+	});
+}
 
 // Listen to the app
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
